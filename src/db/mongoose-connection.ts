bf0c6b5b5d3ad1b3fddea4285397f8e1d296f863
Olivier Riccini <olivier-webdev@hotmail.com>
2019-08-19 18:27:17 -0400

    Ready to be deployed 4

--- src/db/mongoose-connection.ts
+++ src/db/mongoose-connection.ts
@@ -4,20 +4,31 @@ const debug = require('debug')('data-base');
 
 export class MongooseConnection {
     public init() {
-        let i = 1;
-        mongoose.Promise = global.Promise;
-        const operation = retry.operation({retries: 50});
+        console.log('MEGAAAA TESSSSSSSSTTTTT');
+        console.log('************************************************************');
+        console.log('************************************************************');
+        console.log(process.env);
+        console.log('************************************************************');
+        console.log('************************************************************');
+        console.log('************************************************************');
+        console.log('************************************************************');
+        console.log(process.env.NODE_ENV);
+        console.log('************************************************************');
+        console.log('************************************************************');
+        // let i = 1;
+        // mongoose.Promise = global.Promise;
+        // const operation = retry.operation({retries: 50});
 
-        operation.attempt(() => {
-            mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-                .then(() => debug(`Successfully connected DB: ${process.env.MONGODB_URI}`))
-                .catch((err) => {
-                    debug(err);
-                    debug('Reconnection nb = ' + i++ + ' ...');
-                    if (operation.retry(err)) {
-                        return;
-                    }
-                });
-        });
+        // operation.attempt(() => {
+        //     mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+        //         .then(() => debug(`Successfully connected DB: ${process.env.MONGODB_URI}`))
+        //         .catch((err) => {
+        //             debug(err);
+        //             debug('Reconnection nb = ' + i++ + ' ...');
+        //             if (operation.retry(err)) {
+        //                 return;
+        //             }
+        //         });
+        // });
     }
 }
