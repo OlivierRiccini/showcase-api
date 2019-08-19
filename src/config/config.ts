de45d0b2e6c0ef9c8908a958a63a8eeae4812b57
Olivier Riccini <olivier-webdev@hotmail.com>
2019-08-19 18:32:11 -0400

    Ready to be deployed 5

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import * as config from './config.json';
 
 var env = process.env.NODE_ENV || 'development';
-if (env === 'development' || env === 'test') {
+if (env === 'development' || env === 'test' || env === 'production') {
     var envConfig = config[env];
 
     Object.keys(envConfig).forEach((key) => {
--- src/db/mongoose-connection.ts
+++ src/db/mongoose-connection.ts
@@ -4,31 +4,20 @@ const debug = require('debug')('data-base');
 
 export class MongooseConnection {
     public init() {
-        console.log('MEGAAAA TESSSSSSSSTTTTT');
-        console.log('************************************************************');
-        console.log('************************************************************');
-        console.log(process.env);
-        console.log('************************************************************');
-        console.log('************************************************************');
-        console.log('************************************************************');
-        console.log('************************************************************');
-        console.log(process.env.NODE_ENV);
-        console.log('************************************************************');
-        console.log('************************************************************');
-        // let i = 1;
-        // mongoose.Promise = global.Promise;
-        // const operation = retry.operation({retries: 50});
+        let i = 1;
+        mongoose.Promise = global.Promise;
+        const operation = retry.operation({retries: 50});
 
-        // operation.attempt(() => {
-        //     mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
-        //         .then(() => debug(`Successfully connected DB: ${process.env.MONGODB_URI}`))
-        //         .catch((err) => {
-        //             debug(err);
-        //             debug('Reconnection nb = ' + i++ + ' ...');
-        //             if (operation.retry(err)) {
-        //                 return;
-        //             }
-        //         });
-        // });
+        operation.attempt(() => {
+            mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true })
+                .then(() => debug(`Successfully connected DB: ${process.env.MONGODB_URI}`))
+                .catch((err) => {
+                    debug(err);
+                    debug('Reconnection nb = ' + i++ + ' ...');
+                    if (operation.retry(err)) {
+                        return;
+                    }
+                });
+        });
     }
 }
